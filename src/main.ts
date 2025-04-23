ab2f19310afabf8c8cd456b9c14767a5dd6ef3b8
= <=>
2025-04-23 22:42:44 +0000

    Cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,       // 👈 Dynamically reflect the request origin
+    credentials: true   // 👈 Only if you're using cookies or auth headers
+  });
+
   await app.listen(3000);
 }
 bootstrap();
